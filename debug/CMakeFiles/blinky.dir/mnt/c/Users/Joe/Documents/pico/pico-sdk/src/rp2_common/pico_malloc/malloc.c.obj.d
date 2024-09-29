e8450bce44665a30a952cbd39ebc46a871e78e17
vze3f372 <joe.hotchkiss@gmail.com>
2024-09-29 21:25:14 +0300

    initial commit

---
+++ debug/CMakeFiles/blinky.dir/mnt/c/Users/Joe/Documents/pico/pico-sdk/src/rp2_common/pico_malloc/malloc.c.obj.d
@@ -0,0 +1,33 @@
+CMakeFiles/blinky.dir/mnt/c/Users/Joe/Documents/pico/pico-sdk/src/rp2_common/pico_malloc/malloc.c.obj: \
+ /mnt/c/Users/Joe/Documents/pico/pico-sdk/src/rp2_common/pico_malloc/malloc.c \
+ /usr/include/newlib/stdlib.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/_ansi.h /usr/include/newlib/newlib.h \
+ /usr/include/newlib/_newlib_version.h /usr/include/newlib/sys/config.h \
+ /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/machine/stdlib.h /usr/include/newlib/alloca.h \
+ /mnt/c/Users/Joe/Documents/pico/pico-sdk/src/common/pico_base_headers/include/pico.h \
+ /mnt/c/Users/Joe/Documents/pico/pico-sdk/src/common/pico_base_headers/include/pico/types.h \
+ /mnt/c/Users/Joe/Documents/pico/pico-sdk/src/common/pico_base_headers/include/pico/assert.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /usr/include/newlib/assert.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /mnt/c/Users/Joe/Documents/pico/test_project/debug/generated/pico_base/pico/version.h \
+ /mnt/c/Users/Joe/Documents/pico/pico-sdk/src/common/pico_base_headers/include/pico/config.h \
+ /mnt/c/Users/Joe/Documents/pico/test_project/debug/generated/pico_base/pico/config_autogen.h \
+ /mnt/c/Users/Joe/Documents/pico/pico-sdk/src/boards/include/boards/pico.h \
+ /mnt/c/Users/Joe/Documents/pico/pico-sdk/src/rp2_common/cmsis/include/cmsis/rename_exceptions.h \
+ /mnt/c/Users/Joe/Documents/pico/pico-sdk/src/rp2040/pico_platform/include/pico/platform.h \
+ /mnt/c/Users/Joe/Documents/pico/pico-sdk/src/rp2_common/pico_platform_compiler/include/pico/platform/compiler.h \
+ /mnt/c/Users/Joe/Documents/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/platform_defs.h \
+ /mnt/c/Users/Joe/Documents/pico/pico-sdk/src/common/pico_base_headers/include/pico/types.h \
+ /mnt/c/Users/Joe/Documents/pico/pico-sdk/src/rp2_common/pico_platform_sections/include/pico/platform/sections.h \
+ /mnt/c/Users/Joe/Documents/pico/pico-sdk/src/rp2_common/pico_platform_panic/include/pico/platform/panic.h \
+ /mnt/c/Users/Joe/Documents/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/addressmap.h \
+ /mnt/c/Users/Joe/Documents/pico/pico-sdk/src/rp2040/hardware_regs/include/hardware/regs/sio.h \
+ /mnt/c/Users/Joe/Documents/pico/pico-sdk/src/common/pico_base_headers/include/pico/error.h \
+ /mnt/c/Users/Joe/Documents/pico/pico-sdk/src/rp2_common/pico_malloc/include/pico/malloc.h
